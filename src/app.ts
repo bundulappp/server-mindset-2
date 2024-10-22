90721eebca7ea24723684e9feb1f9a79fb871f2a
Bundula Patrik <bundulappp@gmail.com>
2024-10-22 18:50:49 +0200

    add task 5 solution

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,13 @@ export default function createApp(options = {}) {
     reply.send({ hello: 'World!' });
   });
 
-  type BrewSomethingHotParamsType = {
-    Params: {
-      drink: 'coffee' | 'tea' | 'chai';
-    };
-  };
-  app.post<BrewSomethingHotParamsType>(
-    '/api/beverages/:drink',
-    (request, reply) => {
-      const { drink } = request.params;
-
-      reply.send({ drink });
-    }
-  );
-
   type MakeSomethingSoftSweetType = {
     Params: {
       drink: 'coffe' | 'tea' | 'chai';
     };
     Querystring?: {
-      milk: 'yes';
-      sugar: 'yes';
+      milk?: 'yes';
+      sugar?: 'yes';
     };
   };
 
@@ -35,13 +21,21 @@ export default function createApp(options = {}) {
     '/api/beverages/:drink',
     (request, reply) => {
       const { drink } = request.params;
-      const extras = [];
+      const extras: string[] = [];
 
-      if (request.query && 'milk' in request.query) {
+      if (
+        request.query &&
+        'milk' in request.query &&
+        request.query['milk'] === 'yes'
+      ) {
         extras.push('milk');
       }
 
-      if (request.query && 'sugar' in request.query) {
+      if (
+        request.query &&
+        'sugar' in request.query &&
+        request.query['sugar'] === 'yes'
+      ) {
         extras.push('sugar');
       }
 
@@ -53,11 +47,3 @@ export default function createApp(options = {}) {
 
   return app;
 }
-
-// ## Task 5: Make it soft and sweet
-
-// - Extend the `POST /api/beverages/<drink>` (`<drink>` can be coffee, tea or chai) endpoint with a query string. `milk=<yes or no>&sugar=<yes or no>`.
-// - The query params are optional. If some of them missing it is considered as "no".
-// - Extend the reply's body with a `with: []` prop. This array should contains the `'milk'` string if the milk query param was `yes` and similarly the `'sugar'` if the sugar param was `yes`.
-// - Example: `POST /api/beverages/tea?sugar=yes` should respond with the following body in JSON format: `{drink: 'tea', with: ['sugar'] }`.
-// - To test this task issue: `npm test -- task5`
